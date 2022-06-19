a20f913e71deab3bdd86de6077e460840ee5146c
Lucas <46837214+foopsss@users.noreply.github.com>
2022-06-19 03:28:25 -0300

    Third commit
    Added a script to create backups of the config files and a bit larger README.

---
+++ Dotcopy.sh
@@ -0,0 +1,11 @@
+# Defino el intérprete a utilizar.
+#!/bin/bash
+
+# Copio los archivos deseados.
+
+    # Configuración de Sway.
+    cp /var/home/$USER/.config/sway/config /var/home/$USER/Documentos/GitHub/Dotfiles/Sway/config
+    
+    # Configuración de Waybar.
+    cp /var/home/$USER/.config/waybar/config /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/config
+    cp /var/home/$USER/.config/waybar/style.css /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/style.css
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Dotfiles
- My config files for some software.
+ My config files for some software I use, like SwayWM or Waybar. The repository also includes a simple bash script I use to create copies of the config files whenever they are changed.
