07a718d7f92008a352aa09f89f71fa1c9f72d77f
Lucas <46837214+foopsss@users.noreply.github.com>
2022-06-27 09:53:14 -0300

    Dotfiles update.
    Updated files:
    * Sway config.
    * Waybar config.
    * Dotcopy script for adding Mako config files in the future.

--- Dotcopy.sh
+++ Dotcopy.sh
@@ -9,3 +9,6 @@
     # Configuración de Waybar.
     cp /var/home/$USER/.config/waybar/config /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/config
     cp /var/home/$USER/.config/waybar/style.css /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/style.css
+    
+    # Configuración de Mako.
+    cp /var/home/$USER/.config/mako/config /var/home/$USER/Documentos/GitHub/Dotfiles/Mako/config
