04f283099417f64072e6aa7c0a28e46441c72f00
Lucas <46837214+foopsss@users.noreply.github.com>
2022-07-28 00:59:05 -0300

    Updated dotfiles.

--- Dotcopy.sh
+++ Dotcopy.sh
@@ -7,8 +7,11 @@
     cp /var/home/$USER/.config/sway/config /var/home/$USER/Documentos/GitHub/Dotfiles/Sway/config
     
     # Configuración de Waybar.
-    cp /var/home/$USER/.config/waybar/config /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/config
-    cp /var/home/$USER/.config/waybar/style.css /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/style.css
+    # cp /var/home/$USER/.config/waybar/config /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/config
+    # cp /var/home/$USER/.config/waybar/style.css /var/home/$USER/Documentos/GitHub/Dotfiles/Waybar/style.css
     
     # Configuración de Mako.
     cp /var/home/$USER/.config/mako/config /var/home/$USER/Documentos/GitHub/Dotfiles/Mako/config
+
+    # Configuración de Alacritty.
+    cp /var/home/$USER/.config/alacritty/alacritty.yml /var/home/$USER/Documentos/GitHub/Dotfiles/Alacritty/alacritty.yml
